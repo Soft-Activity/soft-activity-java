jscomet/soft-ativity-java

git init: 初始化代码

src/main/java/homwork/soft/activity/ActivityApplication.java
@@ -1,0 +1,13 @@
+package homwork.soft.activity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityApplication.class, args);
+    }
+
+}

src/main/java/homwork/soft/activity/context/CurrentUserContext.java
@@ -1,0 +1,19 @@
+package homwork.soft.activity.context;
+
+import homwork.soft.activity.entity.vo.UserVO;
+
+public class CurrentUserContext {
+    public static ThreadLocal<UserVO> currentUser = new ThreadLocal<>();
+
+    public static UserVO getCurrentUser() {
+        return currentUser.get();
+    }
+
+    public static void setCurrentUser(UserVO user) {
+        currentUser.set(user);
+    }
+
+    public static void removeCurrentUser() {
+        currentUser.remove();
+    }
+}

src/main/java/homwork/soft/activity/entity/dto/AccountQuery.java
@@ -1,0 +1,16 @@
+package homwork.soft.activity.entity.dto;
+
+import homwork.soft.activity.entity.po.Account;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 账号表(Account)查询参数
+ *
+ * @author jscomet
+ * @since 2024-11-24 09:35:29
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class AccountQuery extends Account {
+}

src/main/java/homwork/soft/activity/service/UserService.java
@@ -1,0 +1,45 @@
+package homwork.soft.activity.service;
+
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import homwork.soft.activity.entity.po.User;
+import homwork.soft.activity.entity.dto.UserQuery;
+import homwork.soft.activity.entity.vo.UserVO;
+
+import java.util.List;
+
+/**
+ * 用户(User)表服务接口
+ *
+ * @author jscomet
+ * @since 2024-11-24 11:31:19
+ */
+public interface UserService extends IService<User> {
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param userId 主键
+     * @return 实例对象
+     */
+    UserVO queryById(String userId);
+
+    /**
+     * 查询多条数据
+     *
+     * @param current 查询页面
+     * @param pageSize 查询条数
+     * @param param 查询参数
+     * @return 对象列表
+     */
+    List<UserVO> queryAll(int current, int pageSize, UserQuery param);
+
+    /**
+     * 通过实体作为筛选条件计数
+     *
+     * @param param 查询参数
+     * @return 数量
+     */
+    int count(UserQuery param);
+
+}
+

src/main/java/homwork/soft/activity/service/impl/RoleServiceImpl.java
@@ -1,0 +1,50 @@
+package homwork.soft.activity.service.impl;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.github.pagehelper.PageHelper;
+import homwork.soft.activity.dao.RoleDao;
+import homwork.soft.activity.entity.po.Role;
+import homwork.soft.activity.entity.po.UserRole;
+import homwork.soft.activity.service.RoleService;
+import homwork.soft.activity.entity.dto.RoleQuery;
+import homwork.soft.activity.entity.po.Role;
+import homwork.soft.activity.entity.vo.RoleVO;
+import homwork.soft.activity.service.UserRoleService;
+import org.springframework.stereotype.Service;
+import jakarta.annotation.Resource;
+
+import java.util.List;
+/**
+ * 角色(Role)表服务实现类
+ *
+ * @author jscomet
+ * @since 2024-11-24 13:14:07
+ */
+@Service("roleService")
+public class RoleServiceImpl extends ServiceImpl<RoleDao, Role> implements RoleService {
+    @Resource
+    private RoleDao roleDao;
+
+    @Override
+    public RoleVO queryById(Integer roleId) {
+        return roleDao.queryById(roleId);
+    }
+
+    @Override
+    public List<RoleVO> queryAll(int current, int pageSize, RoleQuery param) {
+        PageHelper.startPage(current, pageSize);
+        return roleDao.queryAll(param);
+    }
+
+    @Override
+    public int count(RoleQuery param) {
+        return roleDao.count(param);
+    }
+
+    @Override
+    public List<Role> queryByUserId(String userId) {
+        return roleDao.queryByUserId(userId);
+    }
+
+
+}
+

src/main/java/homwork/soft/activity/service/impl/UserServiceImpl.java
@@ -1,0 +1,59 @@
+package homwork.soft.activity.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.github.pagehelper.PageHelper;
+import homwork.soft.activity.dao.UserDao;
+import homwork.soft.activity.entity.po.Role;
+import homwork.soft.activity.entity.po.User;
+import homwork.soft.activity.service.RoleService;
+import homwork.soft.activity.service.UserService;
+import homwork.soft.activity.entity.dto.UserQuery;
+import homwork.soft.activity.entity.po.User;
+import homwork.soft.activity.entity.vo.UserVO;
+import org.springframework.stereotype.Service;
+import jakarta.annotation.Resource;
+
+import java.util.List;
+
+/**
+ * 用户(User)表服务实现类
+ *
+ * @author jscomet
+ * @since 2024-11-24 11:31:20
+ */
+@Service("userService")
+public class UserServiceImpl extends ServiceImpl<UserDao, User> implements UserService {
+    @Resource
+    private UserDao userDao;
+    @Resource
+    private RoleService roleService;
+
+    @Override
+    public UserVO queryById(String userId) {
+        UserVO user = userDao.queryById(userId);
+        this.fillVO(user);
+        return userDao.queryById(userId);
+    }
+
+    private void fillVO(UserVO userVO) {
+        String userId = userVO.getUserId();
+        if (userId != null) {
+            List<Role> roles = roleService.queryByUserId(userId);
+            userVO.setRoles(roles);
+        }
+    }
+
+    @Override
+    public List<UserVO> queryAll(int current, int pageSize, UserQuery param) {
+        PageHelper.startPage(current, pageSize);
+        List<UserVO> list = userDao.queryAll(param);
+        list.forEach(this::fillVO);
+        return list;
+    }
+
+    @Override
+    public int count(UserQuery param) {
+        return userDao.count(param);
+    }
+}
+

src/main/java/homwork/soft/activity/util/UploadUtils.java
@@ -1,0 +1,136 @@
+package homwork.soft.activity.util;
+
+import homwork.soft.activity.property.AppProperty;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.Resource;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UploadUtils {
+
+    @Resource
+    private AppProperty _appProperty;
+
+    private static AppProperty appProperty;
+
+    @PostConstruct
+    public void init() {
+        appProperty = this._appProperty;
+    }
+
+    public static String upload(MultipartFile file, String module) throws IOException {
+        // 文件名：日期.UUID.原文件名.后缀
+        String date = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+        String extension = getExtension(file);
+
+        String filename;
+        if (file.getOriginalFilename() != null) {
+            filename = date + "." + uuid + "." + file.getOriginalFilename();
+            // 避免重复后缀
+            if (!filename.endsWith("." + extension)) {
+                filename += "." + extension;
+            }
+        } else {
+            filename = date + "." + uuid + "." + extension;
+        }
+
+        // 上传
+        upload(file, module, filename);
+        return filename;
+    }
+
+    public static void upload(MultipartFile file, String module, String filename) throws IOException {
+        // 判断 module 目录是否存在，不存在时创建
+        String modulePath = getModulePath(module);
+        File moduleDir = new File(modulePath);
+        if (!moduleDir.exists()) {
+            AssertUtils.isTrue(moduleDir.mkdir(), HttpStatus.INTERNAL_SERVER_ERROR, "服务器上传目录异常");
+        }
+
+        // 上传文件
+        String path = getFilePath(module, filename);
+        try {
+            Files.copy(file.getInputStream(), new File(path).toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new IOException( "服务器保存文件异常");
+        }
+    }
+
+    private static String getModulePath(String module) {
+        return new File(appProperty.getBack().getUploadPath() + "/" + module).getPath();
+    }
+
+    public static String getFilePath(String module, String filename) {
+        return new File(getModulePath(module) + "/" + filename).getPath();
+    }
+
+    public static ResponseEntity<org.springframework.core.io.Resource> getResponseEntity(String module, String filename) throws IOException {
+        // 真实文件名
+        String realFileName = filename;
+        // 如果文件名符合自动生成的格式，提取原文件名
+        Pattern pattern = Pattern.compile("^\\d{8}\\.\\w{32}\\..*$");
+        Matcher matcher = pattern.matcher(filename);
+        if (matcher.matches()) {
+            realFileName = filename.substring(42);
+        }
+
+        // 缓存 header
+        String ccValue = CacheControl.maxAge(7, TimeUnit.DAYS)
+                .noTransform()
+                .cachePublic()
+                .getHeaderValue();
+
+        // 判断文件类型
+        MediaType mediaType = MediaType.APPLICATION_OCTET_STREAM;
+        String type = URLConnection.getFileNameMap().getContentTypeFor(filename);
+        if (type != null) {
+            mediaType = MediaType.parseMediaType(type);
+        }
+
+        // 读取文件流并返回
+        File file = new File(getFilePath(module, filename));
+        InputStreamResource resource;
+        try {
+            resource = new InputStreamResource(new FileInputStream(file));
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException(e.getMessage());
+        }
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CACHE_CONTROL, ccValue)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + URLEncoder.encode(realFileName, StandardCharsets.UTF_8) + "\"")
+                .contentLength(file.length())
+                .contentType(mediaType)
+                .body(resource);
+    }
+
+    private static String getExtension(MultipartFile file) {
+        String extension = "";
+        if (file.getOriginalFilename() != null) {
+            extension = FilenameUtils.getExtension(file.getOriginalFilename());
+        }
+        if (StringUtils.isBlank(extension) && file.getContentType() != null) {
+            extension = file.getContentType().substring(file.getContentType().lastIndexOf('/') + 1);
+        }
+        return extension;
+    }
+}

src/main/java/homwork/soft/activity/util/beans/CommonResult.java
@@ -1,0 +1,76 @@
+package homwork.soft.activity.util.beans;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+/**
+ * @param <T> 普通数据响应
+ */
+public class CommonResult<T> extends ResponseEntity<Map<String, Object>> {
+    private int status;
+    private String message;
+    private T data;
+
+    public int getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return this.data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public CommonResult(final int status, final String message, final T data) {
+        super(new HashMap<String, Object>(3) {
+            private static final long serialVersionUID = 8252373729770515446L;
+
+            {
+                this.put("status", status);
+                this.put("message", message);
+                this.put("data", data);
+            }
+        }, HttpStatus.valueOf(status));
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> CommonResult<T> success(T data) {
+        return success(data, HttpStatus.OK.getReasonPhrase());
+    }
+
+    public static <T> CommonResult<T> success(T data, String message) {
+        return new CommonResult(HttpStatus.OK.value(), message, data);
+    }
+
+    public static <T> CommonResult<T> error(HttpStatus status) {
+        return error(status, status.getReasonPhrase());
+    }
+
+    public static <T> CommonResult<T> error(HttpStatus status, String message) {
+        return new CommonResult(status.value(), message, (Object)null);
+    }
+
+    public static <T> CommonResult<T> error(HttpStatus status, String message, T data) {
+        return new CommonResult(status.value(), message, data);
+    }
+}
+

Soft-Activity/soft-activity-java

src/main/java/homework/soft/activity/ActivityApplication.java
@@ -1,0 +1,13 @@
+package homework.soft.activity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityApplication.class, args);
+    }
+
+}

src/main/java/homework/soft/activity/context/CurrentUserContext.java
@@ -1,0 +1,19 @@
+package homework.soft.activity.context;
+
+import homework.soft.activity.entity.vo.UserVO;
+
+public class CurrentUserContext {
+    public static ThreadLocal<UserVO> currentUser = new ThreadLocal<>();
+
+    public static UserVO getCurrentUser() {
+        return currentUser.get();
+    }
+
+    public static void setCurrentUser(UserVO user) {
+        currentUser.set(user);
+    }
+
+    public static void removeCurrentUser() {
+        currentUser.remove();
+    }
+}

src/main/java/homework/soft/activity/entity/dto/AccountQuery.java
@@ -1,0 +1,16 @@
+package homework.soft.activity.entity.dto;
+
+import homework.soft.activity.entity.po.Account;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 账号表(Account)查询参数
+ *
+ * @author jscomet
+ * @since 2024-11-24 09:35:29
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class AccountQuery extends Account {
+}

src/main/java/homework/soft/activity/service/UserService.java
@@ -1,0 +1,45 @@
+package homework.soft.activity.service;
+
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import homework.soft.activity.entity.po.User;
+import homework.soft.activity.entity.dto.UserQuery;
+import homework.soft.activity.entity.vo.UserVO;
+
+import java.util.List;
+
+/**
+ * 用户(User)表服务接口
+ *
+ * @author jscomet
+ * @since 2024-11-24 11:31:19
+ */
+public interface UserService extends IService<User> {
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param userId 主键
+     * @return 实例对象
+     */
+    UserVO queryById(String userId);
+
+    /**
+     * 查询多条数据
+     *
+     * @param current 查询页面
+     * @param pageSize 查询条数
+     * @param param 查询参数
+     * @return 对象列表
+     */
+    List<UserVO> queryAll(int current, int pageSize, UserQuery param);
+
+    /**
+     * 通过实体作为筛选条件计数
+     *
+     * @param param 查询参数
+     * @return 数量
+     */
+    int count(UserQuery param);
+
+}
+

src/main/java/homework/soft/activity/service/impl/RoleServiceImpl.java
@@ -1,0 +1,47 @@
+package homework.soft.activity.service.impl;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.github.pagehelper.PageHelper;
+import homework.soft.activity.dao.RoleDao;
+import homework.soft.activity.entity.po.Role;
+import homework.soft.activity.service.RoleService;
+import homework.soft.activity.entity.dto.RoleQuery;
+import homework.soft.activity.entity.vo.RoleVO;
+import org.springframework.stereotype.Service;
+import jakarta.annotation.Resource;
+
+import java.util.List;
+/**
+ * 角色(Role)表服务实现类
+ *
+ * @author jscomet
+ * @since 2024-11-24 13:14:07
+ */
+@Service("roleService")
+public class RoleServiceImpl extends ServiceImpl<RoleDao, Role> implements RoleService {
+    @Resource
+    private RoleDao roleDao;
+
+    @Override
+    public RoleVO queryById(Integer roleId) {
+        return roleDao.queryById(roleId);
+    }
+
+    @Override
+    public List<RoleVO> queryAll(int current, int pageSize, RoleQuery param) {
+        PageHelper.startPage(current, pageSize);
+        return roleDao.queryAll(param);
+    }
+
+    @Override
+    public int count(RoleQuery param) {
+        return roleDao.count(param);
+    }
+
+    @Override
+    public List<Role> queryByUserId(String userId) {
+        return roleDao.queryByUserId(userId);
+    }
+
+
+}
+

src/main/java/homework/soft/activity/service/impl/UserServiceImpl.java
@@ -1,0 +1,58 @@
+package homework.soft.activity.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.github.pagehelper.PageHelper;
+import homework.soft.activity.dao.UserDao;
+import homework.soft.activity.entity.po.Role;
+import homework.soft.activity.entity.po.User;
+import homework.soft.activity.service.RoleService;
+import homework.soft.activity.service.UserService;
+import homework.soft.activity.entity.dto.UserQuery;
+import homework.soft.activity.entity.vo.UserVO;
+import org.springframework.stereotype.Service;
+import jakarta.annotation.Resource;
+
+import java.util.List;
+
+/**
+ * 用户(User)表服务实现类
+ *
+ * @author jscomet
+ * @since 2024-11-24 11:31:20
+ */
+@Service("userService")
+public class UserServiceImpl extends ServiceImpl<UserDao, User> implements UserService {
+    @Resource
+    private UserDao userDao;
+    @Resource
+    private RoleService roleService;
+
+    @Override
+    public UserVO queryById(String userId) {
+        UserVO user = userDao.queryById(userId);
+        this.fillVO(user);
+        return userDao.queryById(userId);
+    }
+
+    private void fillVO(UserVO userVO) {
+        String userId = userVO.getUserId();
+        if (userId != null) {
+            List<Role> roles = roleService.queryByUserId(userId);
+            userVO.setRoles(roles);
+        }
+    }
+
+    @Override
+    public List<UserVO> queryAll(int current, int pageSize, UserQuery param) {
+        PageHelper.startPage(current, pageSize);
+        List<UserVO> list = userDao.queryAll(param);
+        list.forEach(this::fillVO);
+        return list;
+    }
+
+    @Override
+    public int count(UserQuery param) {
+        return userDao.count(param);
+    }
+}
+

src/main/java/homework/soft/activity/util/UploadUtils.java
@@ -1,0 +1,136 @@
+package homework.soft.activity.util;
+
+import homework.soft.activity.property.AppProperty;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.Resource;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UploadUtils {
+
+    @Resource
+    private AppProperty _appProperty;
+
+    private static AppProperty appProperty;
+
+    @PostConstruct
+    public void init() {
+        appProperty = this._appProperty;
+    }
+
+    public static String upload(MultipartFile file, String module) throws IOException {
+        // 文件名：日期.UUID.原文件名.后缀
+        String date = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+        String extension = getExtension(file);
+
+        String filename;
+        if (file.getOriginalFilename() != null) {
+            filename = date + "." + uuid + "." + file.getOriginalFilename();
+            // 避免重复后缀
+            if (!filename.endsWith("." + extension)) {
+                filename += "." + extension;
+            }
+        } else {
+            filename = date + "." + uuid + "." + extension;
+        }
+
+        // 上传
+        upload(file, module, filename);
+        return filename;
+    }
+
+    public static void upload(MultipartFile file, String module, String filename) throws IOException {
+        // 判断 module 目录是否存在，不存在时创建
+        String modulePath = getModulePath(module);
+        File moduleDir = new File(modulePath);
+        if (!moduleDir.exists()) {
+            AssertUtils.isTrue(moduleDir.mkdir(), HttpStatus.INTERNAL_SERVER_ERROR, "服务器上传目录异常");
+        }
+
+        // 上传文件
+        String path = getFilePath(module, filename);
+        try {
+            Files.copy(file.getInputStream(), new File(path).toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new IOException( "服务器保存文件异常");
+        }
+    }
+
+    private static String getModulePath(String module) {
+        return new File(appProperty.getBack().getUploadPath() + "/" + module).getPath();
+    }
+
+    public static String getFilePath(String module, String filename) {
+        return new File(getModulePath(module) + "/" + filename).getPath();
+    }
+
+    public static ResponseEntity<org.springframework.core.io.Resource> getResponseEntity(String module, String filename) throws IOException {
+        // 真实文件名
+        String realFileName = filename;
+        // 如果文件名符合自动生成的格式，提取原文件名
+        Pattern pattern = Pattern.compile("^\\d{8}\\.\\w{32}\\..*$");
+        Matcher matcher = pattern.matcher(filename);
+        if (matcher.matches()) {
+            realFileName = filename.substring(42);
+        }
+
+        // 缓存 header
+        String ccValue = CacheControl.maxAge(7, TimeUnit.DAYS)
+                .noTransform()
+                .cachePublic()
+                .getHeaderValue();
+
+        // 判断文件类型
+        MediaType mediaType = MediaType.APPLICATION_OCTET_STREAM;
+        String type = URLConnection.getFileNameMap().getContentTypeFor(filename);
+        if (type != null) {
+            mediaType = MediaType.parseMediaType(type);
+        }
+
+        // 读取文件流并返回
+        File file = new File(getFilePath(module, filename));
+        InputStreamResource resource;
+        try {
+            resource = new InputStreamResource(new FileInputStream(file));
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException(e.getMessage());
+        }
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CACHE_CONTROL, ccValue)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + URLEncoder.encode(realFileName, StandardCharsets.UTF_8) + "\"")
+                .contentLength(file.length())
+                .contentType(mediaType)
+                .body(resource);
+    }
+
+    private static String getExtension(MultipartFile file) {
+        String extension = "";
+        if (file.getOriginalFilename() != null) {
+            extension = FilenameUtils.getExtension(file.getOriginalFilename());
+        }
+        if (StringUtils.isBlank(extension) && file.getContentType() != null) {
+            extension = file.getContentType().substring(file.getContentType().lastIndexOf('/') + 1);
+        }
+        return extension;
+    }
+}

src/main/java/homework/soft/activity/util/beans/CommonResult.java
@@ -1,0 +1,76 @@
+package homework.soft.activity.util.beans;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+/**
+ * @param <T> 普通数据响应
+ */
+public class CommonResult<T> extends ResponseEntity<Map<String, Object>> {
+    private int status;
+    private String message;
+    private T data;
+
+    public int getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return this.data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public CommonResult(final int status, final String message, final T data) {
+        super(new HashMap<String, Object>(3) {
+            private static final long serialVersionUID = 8252373729770515446L;
+
+            {
+                this.put("status", status);
+                this.put("message", message);
+                this.put("data", data);
+            }
+        }, HttpStatus.valueOf(status));
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> CommonResult<T> success(T data) {
+        return success(data, HttpStatus.OK.getReasonPhrase());
+    }
+
+    public static <T> CommonResult<T> success(T data, String message) {
+        return new CommonResult(HttpStatus.OK.value(), message, data);
+    }
+
+    public static <T> CommonResult<T> error(HttpStatus status) {
+        return error(status, status.getReasonPhrase());
+    }
+
+    public static <T> CommonResult<T> error(HttpStatus status, String message) {
+        return new CommonResult(status.value(), message, (Object)null);
+    }
+
+    public static <T> CommonResult<T> error(HttpStatus status, String message, T data) {
+        return new CommonResult(status.value(), message, data);
+    }
+}
+