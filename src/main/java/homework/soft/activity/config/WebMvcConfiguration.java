40970715db33c1fe7059756fac8c91b6b0a04374
jscomet <3059741813@qq.com>
2024-12-16 02:05:43 +0800

    fix: 修复swagger文档失败

--- src/main/java/homework/soft/activity/config/WebMvcConfiguration.java
+++ src/main/java/homework/soft/activity/config/WebMvcConfiguration.java
@@ -5,44 +5,47 @@ import homework.soft.activity.interceptor.PermissionAuthorizeInterceptor;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * 配置类，注册web层相关组件
  */
-@Configuration
+@Component
 @Slf4j
-public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+public class WebMvcConfiguration implements WebMvcConfigurer {
     @Resource
     private JwtLoginInterceptor jwtLoginInterceptor;
     @Resource
     private PermissionAuthorizeInterceptor permissionAuthorizeInterceptor;
 
-    /**
-     * 注册自定义拦截器
-     *
-     * @param registry
-     */
-    protected void addInterceptors(InterceptorRegistry registry) {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
         //先根据jwt获取当前用户身份，再对接口的权限进行校验
         registry.addInterceptor(jwtLoginInterceptor)
                 .addPathPatterns("/**");
         registry.addInterceptor(permissionAuthorizeInterceptor)
                 .addPathPatterns("/**");
     }
-
-    /**
-     * 设置静态资源映射，主要是访问接口文档（html、js、css）
-     *
-     * @param registry
-     */
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        log.info("开始设置静态资源映射...");
-        registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //C:\Users\30597\.m2\repository\org\webjars\swagger-ui\5.10.3\swagger-ui-5.10.3.jar!\META-INF\resources\webjars\swagger-ui\5.10.3
+        registry.addResourceHandler("/**").addResourceLocations(
+                "classpath:/static/");
+        registry.addResourceHandler("/swagger-ui.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
+        WebMvcConfigurer.super.addResourceHandlers(registry);
     }
-
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/swagger-ui/")
+                .setViewName("forward:/swagger-ui/index.html");
+    }
 
 }
